65b070f78580a40a7c6c57716e21b6c0914579f4
Foxamurai <Foxamurai@gmail.com>
2021-05-29 23:21:23 +0400

    navigation bar

--- app/src/main/java/com/example/quadroople/MapsActivity.java
+++ app/src/main/java/com/example/quadroople/MapsActivity.java
@@ -1,8 +1,11 @@
 package com.example.quadroople;
 
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -10,8 +13,9 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapsActivity extends FragmentActivity {//implements OnMapReadyCallback {
 
     private GoogleMap mMap;
 
@@ -19,11 +23,36 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
+
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.main_fragment, new MapsFragment()).commit();
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    Fragment selectedFragment = null;
+
+                    switch (item.getItemId()){
+                        case R.id.mapsFragment:
+                            selectedFragment = new MapsFragment();
+                            break;
+                        case R.id.createRouteFragment:
+                            selectedFragment = new CreateRouteFragment();
+                            break;
+                        case R.id.chooseRouteFragment:
+                            selectedFragment = new ChooseRouteFragment();
+                            break;
+                    }
+                    getSupportFragmentManager().beginTransaction().replace(R.id.main_fragment,
+                            selectedFragment).commit();
+
+                    return true;
+                }
+            };
 
     /**
      * Manipulates the map once available.
@@ -34,14 +63,14 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
      * it inside the SupportMapFragment. This method will only be triggered once the user has
      * installed Google Play services and returned to the app.
      */
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
-
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        LatLng asu = new LatLng(46.37805, 48.05291);
-        mMap.addMarker(new MarkerOptions().position(asu).title("Marker in ASU"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(asu,17));
-    }
+//    @Override
+//    public void onMapReady(GoogleMap googleMap) {
+//        mMap = googleMap;
+//
+//        // Add a marker in Sydney and move the camera
+//        LatLng sydney = new LatLng(-34, 151);
+//        LatLng asu = new LatLng(46.37805, 48.05291);
+//        mMap.addMarker(new MarkerOptions().position(asu).title("Marker in ASU"));
+//        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(asu,17));
+//    }
 }
